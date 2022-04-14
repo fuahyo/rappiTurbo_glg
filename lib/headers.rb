ef546b88556b70bb068ae6ede2286a3454638486
justinever9 <justinever9@gmail.com>
2022-04-14 21:22:34 +0700

    update auth token

--- lib/headers.rb
+++ lib/headers.rb
@@ -5,11 +5,11 @@ module ReqHeaders
         "accept-language"=> "es-AR",
         "access-control-allow-headers"=> "*",
         "access-control-allow-origin"=> "*",
-        "app-version"=> "web_v1.15.2",
-        "authorization"=> "Bearer ft.gAAAAABiRa6ZzaQs_9nuCIuLI9W8iEOBrsH0oRcYCp2Gt7zXGSBdp4-OvoMxLwtZTOSTkYxgK5wgHxJj4tEAWiq36bx-aw9XT-pR_LBMDwxF_M6QRT2kSEUgkEQp10GYErjAHQoTu5Eyf7e1vHBDdotqv7rcLYMGBcE9TEpsU8kByJdFVmn2aFOOMY8aj4Lv8eDibNGmi4si6fG7ApP3AX-L7OzUtaasGjrYyQ_NL4vyVAKzQqaUfJf_GG7m0EZMO9RQTO8i2n7c_LiJFOgBQYOor-qGxKUKOCJzraNSYBcw7XP0z9SRyEy_k2kbBwrcbRXbogvY3dwEasCCZoI34u2P-edLDDQm-wlHOFVlaGSwvyFhUAkIXqA=",
+        "app-version"=> "web_v1.17.2",
+        "authorization"=> "Bearer ft.gAAAAABiWC19UWmF8FfEzMgpMBKYzt8mQrRDQN7aZ3rEg8TogqFmaJuQZPQTb7W5JOXE4yj_Y_BBhEUdCGAZ6Sql6N5s42W-a6zjAhbK3p0aAhHaCF6CMEl0B3kVsVYiys_ZANhGf9GxF7xkWiHb3UaFJOrJisg0QHtofqJ4SkZ5G4YFJqYrcrin8cHS5xxSwYM7bIe_Kwg0gtUwQde-piwoAdai_Vz0_cP2YMZXTR9uRCfoq0ykomrvf59OiKg6B62haj2F8dF5dXqJDBIPaLPkNtikYWUhe0GZxjTwBqp5wYHelaEj_ZYeGhBJapruaimFZBZGqfY_S--4Mn2nqs0Vbqc0tyx0AKJahtGBXaf4jRObltBdJIA=",
         # "content-length"=>203,
-        "content-type"=> "application/json; charset=UTF-8",
-        "deviceid"=> "9a431c59-3369-4046-a93d-e9a6477d7fa4",
+        "content-type"=> "application/json",
+        "deviceid"=> "d816e7e6-4f69-4c86-ac3a-09d484b9cd04",
         "ignorerefreshtoken"=> "false",
         "ignoretoken"=> "false",
         "include_context_info"=> "true",
@@ -17,7 +17,7 @@ module ReqHeaders
         "needappsflyerid"=> "false",
         "origin"=> "https://www.rappi.com.ar",
         "referer"=> "https://www.rappi.com.ar",
-        "user-agent"=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36"
+        "user-agent"=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
     }
     
 end
