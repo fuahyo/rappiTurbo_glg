2f62d7503401f8d75533d1a50eea5a32a7ef74fe
justinever9 <justinever9@gmail.com>
2022-08-15 22:13:49 +0700

    update header

--- lib/headers.rb
+++ lib/headers.rb
@@ -9,7 +9,7 @@ module ReqHeaders
         "authorization"=> "Bearer ft.gAAAAABi-l-eBTMvMjlFRi1J7pNCBBelDLK2QbKEViZ8PO83kF8nij6DXRlsX-czhdLf6Q6WcOb1hu5CVOruaQJKIN4AHZTh3Zo0Sr8U6Mbmbmbu-tHwmI-eaf-6pae3JbbLYKMxhUXEBw011AMiAwuz46r1ar3CemvtbKyGLccMddybscb7n-H5Cg-kNB2GlWGMvZFBWp1Y2s4--XoUWVoV1IJy2iUSMvORK_SpbL7bud_Wxl_fxCwi5XXn2GJr20DAXn0scsKg1FM15VF9_r7hG0I3vNPXtSb_jgI35tv0gRXPprrpgbrTWDdQpH5o3HEiB1QfqRAJivDhoUL16X7OKW3C6X8djFCgrD8iYg854PT6J11ZWNk=",
         # "content-length"=>203,
         "content-type"=> "application/json",
-        "deviceid"=> "4ce76cdc-053a-4f0e-8a0b-9c2e671a8a62",
+        "deviceid"=> "88a81096-b360-4b29-b135-a6ac9de4656c",
         # "ignorerefreshtoken"=> "false",
         # "ignoretoken"=> "false",
         "include_context_info"=> "true",
@@ -17,7 +17,7 @@ module ReqHeaders
         "needappsflyerid"=> "false",
         "origin"=> "https://www.rappi.com.ar",
         "referer"=> "https://www.rappi.com.ar",
-        "user-agent"=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+        "user-agent"=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
     }
     
 end
