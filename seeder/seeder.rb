bb09e2ddbe6ff2778557fe405b56e86899399d7a
justinever9 <justinever9@gmail.com>
2022-09-19 15:56:04 +0700

    fix seeder file

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -5,7 +5,7 @@ lng = ENV['location_lng']
 
 ## USING WEB API
 body = '{"dynamic_list_request":{"limit":10,"offset":0,"context":"store_home","state":{"lat":"'+lat.to_s+'","lng":"'+lng.to_s+'"}},"dynamic_list_endpoint":"context/content","proxy_input":{"seo_friendly_url":"185340-turbo-aab"}}'
-to_queue << {
+to_queue = {
     url: "https://services.rappi.com.ar/api/ms/web-proxy/dynamic-list/cpgs/",
     page_type: 'aisle',
     body: body,
