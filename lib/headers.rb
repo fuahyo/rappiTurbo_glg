548cae4ed8454e0c611f75716039cd944de6afc6
justinever9 <justinever9@gmail.com>
2022-10-11 23:37:53 +0700

    update header

--- lib/headers.rb
+++ lib/headers.rb
@@ -5,11 +5,11 @@ module ReqHeaders
         "accept-language"=> "es-AR",
         "access-control-allow-headers"=> "*",
         "access-control-allow-origin"=> "*",
-        "app-version"=> "web_v1.62.1",
-        "authorization"=> "Bearer ft.gAAAAABjMv0xFSo7mGGu_Jm_3bH5h8PRsvOWEeG5qllWtDunArQoNJ_fq7GNb-3PJWGk2xo1EiD4xGR7Ixo7vl_vUhbYJmWd3S3w6nWJbBj4IrnR2E8DMA_dT2zKKmShfPJ-gSisQ9hILudFvQnOnU0smhrMVsLaAscfno5luGzlPFRTSsc7b0pIph__mpVRpaG6XYgN17omSr_-lYG6daMPgmshTflNu7uS6BUr1pBMB_6DqCSmGV_rf54wCj-MOTasuek7hvyJteO84l7nyMH-xxOtqDKoi8uU5a_IRRBDzbtX3f80tnW5RR3e-lWIZYfCOVhMuftO00C0Q5SH7fP7sWIEsWz3dOYZ5i8pcWONp9Nmhf5fdY0=",
+        "app-version"=> "web_v1.71.0",
+        "authorization"=> "Bearer ft.gAAAAABjRZun_bHxZpMH_Kw4ld1IwNNJVuZcaM5-pGoGjV5eFyns9nE1rzXtjN1Hk3QRpEphpCtYIGWqFGrmgB9JGFkEsgmeDkppBXtUIiZYf6w_nv4klN_OKLBzSvwCfb6jll7TnkEwJmrP6OVHQ09TB1qRuBI1WsMHgxjuxfLXu1D08mmBlPK41kpsdte2Lz8xxGmyAKf3-4_aruhIQf_Y4Xw1kMv4nkikVAQrR3-wnGetsrmdcU0pO5JK6DTnsi4ACa-VweN0SWhPFCzJffViVeJlkm4wU8QbfuI6s9NC5HQupadgUASKNZ3smsvm68Zr5EFrULPHynXxFYlEsHMqXPjGaDZaoZfICcEOzjEMNEFes3QdjVg=",
         # "content-length"=>203,
         "content-type"=> "application/json",
-        "deviceid"=> "92d6435e-24aa-4da9-a0f0-eac5f862e684",
+        "deviceid"=> "ace03fb6-37f2-4f45-8015-8f50c7fd77ea",
         # "ignorerefreshtoken"=> "false",
         # "ignoretoken"=> "false",
         "include_context_info"=> "true",
@@ -17,7 +17,7 @@ module ReqHeaders
         "needappsflyerid"=> "false",
         "origin"=> "https://www.rappi.com.ar",
         "referer"=> "https://www.rappi.com.ar",
-        "user-agent"=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+        "user-agent"=> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
     }
     
 end
