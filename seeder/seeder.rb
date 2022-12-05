99550c186ce5a530bc9363712fe7e796ec7e2edf
justinever9 <justinever9@gmail.com>
2022-12-05 19:51:39 +0700

    test categories

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,6 +1,7 @@
 pages << {
     page_type: "categories",
-    url: "https://www.rappi.com.ar/tiendas/185340-turbo-aab/catalogo",
+    # url: "https://www.rappi.com.ar/tiendas/185340-turbo-aab/catalogo",
+    url: "https://www.rappi.com.ar/tiendas/185340-turbo-aab/listo-para-comer",
     headers: {
         "cookie"=>"currentLocation=eyJhZGRyZXNzIjoiQnVlbm9zIEFpcmVzLCBBcmdlbnRpbmEiLCJzZWNvbmRhcnlMYWJlbCI6IkFyZ2VudGluYSIsImRpc3RhbmNlSW5LbXMiOjQuOCwicGxhY2VJZCI6IkNoSUp2UXo1VGp2S3ZKVVJoNDdvaUM2QnM2QSIsInBsYWNlSW5mb3JtYXRpb24iOm51bGwsInNvdXJjZSI6ImNvcmVtYXBzIiwiaWQiOjEsImRlc2NyaXB0aW9uIjoiIiwibGF0IjotMzQuNjAzNjg0NCwibG5nIjotNTguMzgxNTU5MSwiY291bnRyeSI6IkFyZ2VudGluYSIsImFjdGl2ZSI6dHJ1ZSwiaXNJbml0aWFsTG9jYXRpb24iOnRydWV9;"
     }
