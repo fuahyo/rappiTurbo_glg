0ad2bff1a4f8b0a3b2b6c1e5f6bebd9187ac2754
justinever9 <justinever9@gmail.com>
2022-12-15 00:17:32 +0700

    update categories selector

--- parsers/categories.rb
+++ parsers/categories.rb
@@ -1,13 +1,24 @@
 html = Nokogiri::HTML(content)
 
-categories = html.css(".lioYQK ul li")
-# script = JSON.parse(html.at("script#__NEXT_DATA__"))
-i = 1
+categories = html.css(".lioYQK ul li.ldHzLE")
 
-cat_id = ''
 categories.each_with_index do |category, idx|
-    cat = category.at_css("a[data-testid=\"typography\"]")
+    cat = category.at_css("h3[data-testid=\"typography\"]")
     subcategories = category.css('ul li a')
+
+    if subcategories.empty?
+        pages << {
+            page_type: "listings",
+            url: "https://www.rappi.com.ar#{cat.attr('href')}",
+            headers: page['headers'],
+            no_redirect: true,
+            vars: {
+                cat: cat.text,
+                sub_cat: nil,
+                page_number: 1
+            }
+        }
+    end
 
     subcategories.each do |subcategory|
         
@@ -17,22 +28,10 @@ categories.each_with_index do |category, idx|
             headers: page['headers'],
             no_redirect: true,
             vars: {
-                cat: subcategory['href'].split('/').last,
+                cat: cat.text,
+                sub_cat: subcategory.text,
                 page_number: 1
             }
         }
     end
-    
-    i = i+1
-
-    # pages << {
-    #     page_type: "sidebar_categories",
-    #     url: "https://www.rappi.com.ar#{cat['href']}",
-    #     headers: page['headers'],
-    #     no_redirect: true,
-    #     vars: {
-    #         cat: cat.text,
-    #         page_number: 1
-    #     }
-    # }
 end
