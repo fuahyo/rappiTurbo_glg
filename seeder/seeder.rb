c917c563b7bd0aabd5508f9b8ef052c4a11ac60e
justinever9 <justinever9@gmail.com>
2023-07-07 22:29:14 +0700

    test header

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,8 +3,11 @@ pages << {
     url: "https://www.rappi.com.ar/tiendas/185340-turbo-aab/catalogo",
     # url: "https://www.rappi.com.ar/tiendas/185340-turbo-aab/listo-para-comer",
     headers: {
+        "Accept-Encoding" => "gzip, deflate, br",
+        "Accept-Language" => "en-US,en;q=0.9",
+        "Cache-Control" => "max-age=0",
         "user-agent"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
         'accept'=>"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "cookie"=>"currentLocation=eyJhZGRyZXNzIjoiQnVlbm9zIEFpcmVzLCBBcmdlbnRpbmEiLCJzZWNvbmRhcnlMYWJlbCI6IkFyZ2VudGluYSIsImRpc3RhbmNlSW5LbXMiOjQuOCwicGxhY2VJZCI6IkNoSUp2UXo1VGp2S3ZKVVJoNDdvaUM2QnM2QSIsInBsYWNlSW5mb3JtYXRpb24iOm51bGwsInNvdXJjZSI6ImNvcmVtYXBzIiwiaWQiOjEsImRlc2NyaXB0aW9uIjoiIiwibGF0IjotMzQuNjAzNjg0NCwibG5nIjotNTguMzgxNTU5MSwiY291bnRyeSI6IkFyZ2VudGluYSIsImFjdGl2ZSI6dHJ1ZSwiaXNJbml0aWFsTG9jYXRpb24iOnRydWV9;"
+        # "cookie"=>"currentLocation=eyJhZGRyZXNzIjoiQnVlbm9zIEFpcmVzLCBBcmdlbnRpbmEiLCJzZWNvbmRhcnlMYWJlbCI6IkFyZ2VudGluYSIsImRpc3RhbmNlSW5LbXMiOjQuOCwicGxhY2VJZCI6IkNoSUp2UXo1VGp2S3ZKVVJoNDdvaUM2QnM2QSIsInBsYWNlSW5mb3JtYXRpb24iOm51bGwsInNvdXJjZSI6ImNvcmVtYXBzIiwiaWQiOjEsImRlc2NyaXB0aW9uIjoiIiwibGF0IjotMzQuNjAzNjg0NCwibG5nIjotNTguMzgxNTU5MSwiY291bnRyeSI6IkFyZ2VudGluYSIsImFjdGl2ZSI6dHJ1ZSwiaXNJbml0aWFsTG9jYXRpb24iOnRydWV9;"
     }
 }   
