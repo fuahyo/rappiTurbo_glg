3cde6db9e9edd4f536685d0f648952d6d8827eaa
justinever9 <justinever9@gmail.com>
2024-02-06 03:06:34 +0700

    update temporary

--- finisher/finisher.rb
+++ finisher/finisher.rb
@@ -1,6 +1,53 @@
+# per_page = 500
+# last_id = ''
+# locations = []
+
+# while true
+#   query = {
+#     '_id' => {'$gt' => last_id},
+#     '$orderby' => [{'_id' => 1}]
+#   }
+#   records = find_outputs("products", query, 1, per_page)
+
+#   records.each do |prod|
+#     if prod['category'] == 'Congelados'
+#         prod['category_id_parent'] = '2878'
+#     elsif prod['category'] == 'Bebidas'
+#         prod['category_id_parent_parent'] = '2807'
+#     elsif prod['category'] == 'Quesos y fiambres'
+#         prod['category_id_parent'] = '2924'
+#     elsif prod['category'] == 'Snacks y Galletitas'
+#         prod['category_id_parent'] = '2933'
+#     elsif prod['category'] == 'Frescos y lácteos'
+#         prod['category_id_parent'] = '2882'
+#     elsif prod['category'] == 'Bebidas alcohólicas'
+#         prod['category_id_parent'] = '2814'
+#     elsif prod['category'] == 'Listo para comer'
+#         prod['category_id_parent'] = '2905'
+#     elsif prod['category'] == 'Tabaco'
+#         prod['category_id_parent'] = '2938'
+#     elsif prod['category'] == 'Helados y Postres'
+#         prod['category_id_parent'] = '3774'
+#     elsif prod['category'] == 'Chocolates y Golosinas'
+#         prod['category_id_parent'] = '3923'
+#     elsif prod['category'] == 'Vinos'
+#         prod['category_id_parent'] = '3919'
+#     elsif prod['category'] == 'Ofertas'
+#         prod['category_id_parent'] = '3773'
+#     end
+#     outputs << prod
+
+#     save_outputs(outputs) if outputs.count > 99
+#   end
+
+#   break if records.nil? || records.count < 1
+
+#   last_id = records.last['_id']
+# end
+
 per_page = 500
 last_id = ''
-locations = []
+items = []
 
 while true
   query = {
@@ -9,38 +56,17 @@ while true
   }
   records = find_outputs("products", query, 1, per_page)
 
-  records.each do |prod|
-    if prod['category'] == 'Congelados'
-        prod['category_id_parent'] = '2878'
-    elsif prod['category'] == 'Bebidas'
-        prod['category_id_parent_parent'] = '2807'
-    elsif prod['category'] == 'Quesos y fiambres'
-        prod['category_id_parent'] = '2924'
-    elsif prod['category'] == 'Snacks y Galletitas'
-        prod['category_id_parent'] = '2933'
-    elsif prod['category'] == 'Frescos y lácteos'
-        prod['category_id_parent'] = '2882'
-    elsif prod['category'] == 'Bebidas alcohólicas'
-        prod['category_id_parent'] = '2814'
-    elsif prod['category'] == 'Listo para comer'
-        prod['category_id_parent'] = '2905'
-    elsif prod['category'] == 'Tabaco'
-        prod['category_id_parent'] = '2938'
-    elsif prod['category'] == 'Helados y Postres'
-        prod['category_id_parent'] = '3774'
-    elsif prod['category'] == 'Chocolates y Golosinas'
-        prod['category_id_parent'] = '3923'
-    elsif prod['category'] == 'Vinos'
-        prod['category_id_parent'] = '3919'
-    elsif prod['category'] == 'Ofertas'
-        prod['category_id_parent'] = '3773'
+  records['data'].each do |product|
+    # require 'byebug'; byebug
+    unless product['competitor_product_id'] == "185340_2113858933"
+        product['_collection'] = "new_products"
     end
-    outputs << prod
+    outputs << product
 
     save_outputs(outputs) if outputs.count > 99
   end
 
-  break if records.nil? || records.count < 1
+  break if records['data'].nil? || records['data'].count < 1
 
-  last_id = records.last['_id']
+  last_id = records['data'].last['_id']
 end
